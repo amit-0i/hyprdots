bbb81e3ec9f7723c96acef361c109b55cd4f85dd
amit-0i <6329amit@gmail.com>
2026-03-28 23:47:46 +0530

    remove: drop flatpaks entirely

--- Scripts/install_pst.sh
+++ Scripts/install_pst.sh
@@ -53,22 +53,3 @@ fi
 
 # shell
 "${scrDir}/restore_shl.sh"
-
-# flatpak
-if ! pkg_installed flatpak; then
-
-    echo -e "\033[0;32m[FLATPAK]\033[0m flatpak application list..."
-    awk -F '#' '$1 != "" {print "["++count"]", $1}' "${scrDir}/.extra/custom_flat.lst"
-    prompt_timer 60 "Install these flatpaks? [Y/n]"
-    fpkopt=${promptIn,,}
-
-    if [ "${fpkopt}" = "y" ]; then
-        echo -e "\033[0;32m[FLATPAK]\033[0m installing flatpaks..."
-        "${scrDir}/.extra/install_fpk.sh"
-    else
-        echo -e "\033[0;33m[SKIP]\033[0m installing flatpaks..."
-    fi
-
-else
-    echo -e "\033[0;33m[SKIP]\033[0m flatpak is already installed..."
-fi
